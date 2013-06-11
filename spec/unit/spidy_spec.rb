8b7c1f7e03c8f356756447d168d0c7fb590b83c9
Rahul Jayaraman <recklessrahul@gmail.com>
2013-06-11 13:17:40 +0530

    refactor tests

--- spec/unit/spidy_spec.rb
+++ spec/unit/spidy_spec.rb
@@ -4,22 +4,20 @@ class Agent; end
 
 describe Spidy do
   let(:spider) { Spidy.new "Test" }
+  let(:action1) { stub(action_type: :visit, url: "http://google.com") }
+  let(:action2) { stub(action_type: :form, field_name: "q", text: "Apple") }
+  let(:action3) { stub(action_type: :yank, div: "resultStats") }
 
   it "should respond to name" do
     spider.name.should == "Test" 
   end
 
   it "should allow adding actions to its web" do
-    action1 = stub(action_type: :visit)
-    action2 = stub(action_type: :yank)
     spider.add_to_web(action1, action2)
     spider.actions.should == [action1, action2]
   end
 
   it "should delegate actions correctly when asked to crawl" do
-    action1 = stub(action_type: :visit, url: "http://google.com")
-    action2 = stub(action_type: :form, field_name: "q", text: "Apple")
-    action3 = stub(action_type: :yank, div: "resultStats")
     spider.add_to_web(action1, action2, action3)
     spider.should_receive(:visit).with(action1).ordered
     spider.should_receive(:form).with(action2).ordered
@@ -28,7 +26,6 @@ describe Spidy do
   end
 
   it "should not crawl without a yank" do
-    action1 = stub(action_type: :visit)
     spider.add_to_web(action1)
     expect { spider.crawl }.
       to raise_error YankNotAvailableError
