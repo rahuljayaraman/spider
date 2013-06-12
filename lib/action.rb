76e9c37db1a9a1f07d8069cb84b2c4623e5977aa
Rahul Jayaraman <recklessrahul@gmail.com>
2013-06-12 09:54:56 +0530

    fix bug: could not read action attributes

--- lib/action.rb
+++ lib/action.rb
@@ -1,4 +1,6 @@
 class Action
+  attr_accessor :action_type, :url, :form_name, :field_name, :text, :div
+
   def initialize params
     @action_type = params.fetch :action_type
     case @action_type
@@ -7,6 +9,9 @@ class Action
     when :fill_form
       @form_name = params.fetch :form_name
       @field_name = params.fetch :field_name
+      @text = params.fetch :text
+    when :yank_data
+      @div = params.fetch :div
     end
   end
 end
